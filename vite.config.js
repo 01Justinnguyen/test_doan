c0ea58ada15d5194f811df3e391dab4ce26a5d80
Liam <phuc01112002@gmail.com>
2023-12-06 23:02:45 +0700

    config import

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '~', replacement: '/src' }]
+  }
 })
 
 // import { defineConfig } from 'vite'
